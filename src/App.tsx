cc7f19d01c2a22d42a23f4e3991575ae61e29f93
mahmoud661 <mahmoudzuriqi8@gmail.com>
2025-06-11 13:38:26 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+
+export default function App() {
+  // Creates a new editor instance.
+  const editor = useCreateBlockNote();
+  console.log("Editor instance created:", editor);
+  // Renders the editor instance using a React component.
+  return <BlockNoteView editor={editor} theme={'light'} className="min-h-11"/>;
+}
