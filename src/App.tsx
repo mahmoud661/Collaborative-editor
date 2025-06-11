569687caf84741eff947158143a7ef4278019f26
mahmoud661 <mahmoudzuriqi8@gmail.com>
2025-06-11 13:43:54 +0300

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
-
+import Header from "./components/Header";
 export default function App() {
   // Creates a new editor instance.
   const editor = useCreateBlockNote();
   console.log("Editor instance created:", editor);
   // Renders the editor instance using a React component.
-  return <BlockNoteView editor={editor} theme={'light'} className="min-h-11"/>;
+  return (
+    <>
+      <Header />
+      <BlockNoteView editor={editor} theme={"light"} className="min-h-11" />
+    </>
+  );
 }
+
